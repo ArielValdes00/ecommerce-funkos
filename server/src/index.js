401da803ce03cefcea8c3e2cdbdd04978ae6b779
ArielValdes00 <124757872+ArielValdes00@users.noreply.github.com>
2023-06-28 15:21:43 -0300

    Se arreglo la url del puerto

--- server/src/index.js
+++ server/src/index.js
@@ -5,7 +5,7 @@ import { PORT } from './config.js';
 async function main() {
     try {
         await sequelize.sync()
-        app.listen('https://ecommerce-funkos-9nrur4xy2-arielvaldes00.vercel.app/');
+        app.listen(PORT);
         console.log(`server is listening on port ${PORT}`);
     } catch (error) {
         console.log(error)
