df90c2cfb4ffc58df744ab4c6165bdf8fbb08773
ArielValdes00 <124757872+ArielValdes00@users.noreply.github.com>
2023-08-25 19:42:18 -0300

    Añadidas etiquetas meta.
    Se han introducido las etiquetas meta necesarias en el archivo HTML para proporcionar una descripción precisa de la tienda en línea de Funko Pop. Estas nuevas etiquetas brindan información detallada sobre la variedad de figuras Funko Pop disponibles, las opciones de pago seguras a través de PayPal y la inclusión de un tablero intuitivo para el seguimiento de pedidos. Además, se han ajustado las palabras clave para resaltar de manera efectiva las características más relevantes de la tienda.

--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -1,11 +1,26 @@
 import '@/styles/globals.css'
 import { SessionProvider } from 'next-auth/react';
 import { ProductProvider } from '../context/ProductContext.js';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
     return (
         <ProductProvider>
             <SessionProvider>
+            <Head>
+                <title>Porfolio</title>
+                <link rel="icon" href="/logo-title.png" />
+                <meta name="description" content="Welcome to my Full Stack Developer portfolio. I am Ariel Valdés, a passionate Full Stack Developer with expertise in building web applications. Here you can explore some of my projects and works, showcasing my skills in front-end and back-end development, as well as database management. Feel free to browse around and get in touch if you're interested in collaborating on exciting projects!" />
+                <meta property="og:title" content="Full Stack Developer Portfolio - Ariel Valdés" />
+                <meta property="og:description" content="Welcome to my Full Stack Developer portfolio. I am Ariel Valdés, a passionate Full Stack Developer with expertise in building web applications. Here you can explore some of my projects and works, showcasing my skills in front-end and back-end development, as well as database management. Feel free to browse around and get in touch if you're interested in collaborating on exciting projects!" />
+                <meta property="og:image" content="https://res.cloudinary.com/dnczjmsbt/image/upload/v1691437299/bg-portfolio_f0unhu.png" />
+                <meta property="og:url" content="https://portfolio-ochre-six-55.vercel.app/" />
+                <meta property="og:type" content="website" />
+                <meta property="og:locale" content="en_US" />
+                <meta property="og:site_name" content="Full Stack Developer Portfolio - Ariel Valdés" />
+                <meta name="keywords" content="full stack developer, web development, front-end, back-end, database management, projects, works, react, next.js, tailwind, javascript" />
+                <meta name="author" content="Ariel Valdés" />
+            </Head>
                 <Component {...pageProps} />
             </SessionProvider>
         </ProductProvider>
--- server/src/config.js
+++ server/src/config.js
@@ -1,8 +1,2 @@
 export const PORT = process.env.PORT || 5000
 
-export const DB_HOST = 'localhost'
-export const DB_USER = 'root'
-export const DB_PASSWORD = 'arielvaldes0102'
-export const DB_NAME =  'ecommerce_funko'
-export const DB_PORT =  '3306'
-
