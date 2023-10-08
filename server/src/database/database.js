f17cdaa53617ef00bcf4db4a56d308ba4f98cdef
ArielValdes00 <124757872+ArielValdes00@users.noreply.github.com>
2023-10-08 15:48:50 -0300

    Se agregó el dialecto pg en la configuración de la base de datos.

--- server/src/database/database.js
+++ server/src/database/database.js
@@ -1,3 +1,4 @@
+import pg from 'pg';
 import Sequelize from "sequelize";
 import dotenv from 'dotenv';
 dotenv.config();
@@ -9,6 +10,7 @@ export const sequelize = new Sequelize(process.env.DATABASE_URL, {
             require: true,
             rejectUnauthorized: false
         }
-    }
+    },
+    dialectModule: pg
 })
 
