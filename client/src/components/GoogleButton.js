8866e615018b4c4a50f5c9f4c6f55a0a1b33e87e
ArielValdes00 <124757872+ArielValdes00@users.noreply.github.com>
2023-10-08 13:27:58 -0300

    Se cambió la forma de importar las imágenes en el componente de GoogleButton y InputFile.

--- client/src/components/GoogleButton.js
+++ client/src/components/GoogleButton.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Image from 'next/image';
-import Google from '../../public/icons/google.png';
 import { signIn } from 'next-auth/react';
 
 const GoogleButton = () => {
@@ -10,7 +9,7 @@ const GoogleButton = () => {
             className="flex gap-3 items-center justify-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             type="button"
         >
-            <Image src={Google} height={20} width={20} alt='Google'></Image>
+            <Image src={'/public/icons/google.png'} height={20} width={20} alt='Google'></Image>
             Sign in with Google
         </button>
     )
--- client/src/components/dashboardComponents/InputFile.js
+++ client/src/components/dashboardComponents/InputFile.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import UploadImage from '/public/icons/upload-image.png';
 import Image from 'next/image';
 
 const InputFile = ({ imageRef, handleImageChange, labelName, selectedImage }) => {
@@ -8,7 +7,7 @@ const InputFile = ({ imageRef, handleImageChange, labelName, selectedImage }) =>
     return (
         <div className="mt-2 text-center border-dashed border-2 border-gray-300 py-3 pt-4">
             <label htmlFor={inputId} className="block cursor-pointer">
-                <Image src={UploadImage} height={60} width={60} alt='Upload Image'
+                <Image src={'/public/icons/upload-image.png'} height={60} width={60} alt='Upload Image'
                     className='mx-auto mb-2' />
                 <input
                     type="file"
